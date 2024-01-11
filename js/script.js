5f7515762840003d3149d90a341fa96232947144
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-01-11 20:43:01 -0300

    criação da pagina de checkin e checkout

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function submitReservation() {
+  var checkInDate = document.getElementById('checkInDate').value;
+  var checkOutDate = document.getElementById('checkOutDate').value;
+
+ 
+  console.log('Check-in:', checkInDate);
+  console.log('Check-out:', checkOutDate);
+
+}
